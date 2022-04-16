2c24cd5dc82021006ac6c44b6c423fb2aeea616a
Gouda <essamgouda97@hotmail.com>
2022-04-16 06:15:14 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Simple RGB image viewer (CLI)
+Zoom and pan by: https://www.reddit.com/r/pygame/comments/drzukj/i_made_a_simple_zoomandpan_program_for_images_but/
+
+Structured, CLI interface and RGB picker by me.
+
+Simple CLI tool to view image and check the RGB value per pixel. More tests to be added and improved tool to be created. (Part of my Journey to learn pygame).
+
+# 
+Python required 3.6+
+```
+git clone https://github.com/essamgouda97/rgb-image-viewer
+cd rgb-image-viewer
+python -m venv ./venv/
+. ./venv/bin/activate
+pip install -e .
+```
+
+# USAGE
+Using command line tool:
+```
+rgb-image-viewer -p '/path/to/image.png'
+```
+
+Using python script:
+```
+python main.py -p './path/to/image.png'
+```
+
---
+++ rgb_img_viewer/main.py
@@ -0,0 +1,137 @@
+import os.path
+import pygame as pg
+
+# Place a picture called "sheet.png" in the same folder as this program!
+# Zoom with mousewheel, pan with left mouse button
+# Print a snapshot of the screen with "P"
+
+import argparse
+parser = argparse.ArgumentParser(description="View image RGBA")
+parser.add_argument('-p', '--path', required=True, type=str)
+
+args = parser.parse_args()
+
+sprite_sheet = pg.image.load(args.path)
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pg.time.Clock()
+zoom_event = False
+scale_up = 1.2
+scale_down = 0.8
+
+
+class GameState:
+    def __init__(self):
+        self.tab = 1
+        self.zoom = 1
+        self.world_offset_x = 0
+        self.world_offset_y = 0
+        self.update_screen = True
+        self.panning = False
+        self.pan_start_pos = None
+        self.legacy_screen = pg.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+game_state = GameState()
+
+
+def world_2_screen(world_x, world_y):
+    screen_x = (world_x - game_state.world_offset_x) * game_state.zoom
+    screen_y = (world_y - game_state.world_offset_y) * game_state.zoom
+    return [screen_x, screen_y]
+
+
+def screen_2_world(screen_x, screen_y):
+    world_x = (screen_x / game_state.zoom) + game_state.world_offset_x
+    world_y = (screen_y / game_state.zoom) + game_state.world_offset_y
+    return [world_x, world_y]
+
+
+# game loop
+try:
+    while 1:
+        # Banner FPS
+        # pg.display.set_caption('(%d FPS)' % (clock.get_fps()))
+        # Mouse screen coords
+        mouse_x, mouse_y = pg.mouse.get_pos()
+
+        # event handler
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_RETURN:
+                    if game_state.tab == 1:
+                        game_state.tab = 2
+                    elif game_state.tab == 2:
+                        game_state.tab = 1
+                elif event.key == pg.K_p:
+                    pg.image.save(screen, "NEW.png")
+
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 4 or event.button == 5:
+                    # X and Y before the zoom
+                    mouseworld_x_before, mouseworld_y_before = screen_2_world(mouse_x, mouse_y)
+
+                    # ZOOM IN/OUT
+                    if event.button == 4 and game_state.zoom < 50:
+                        game_state.zoom *= scale_up
+                    elif event.button == 5 and game_state.zoom > 0.5:
+                        game_state.zoom *= scale_down
+
+                    # X and Y after the zoom
+                    mouseworld_x_after, mouseworld_y_after = screen_2_world(mouse_x, mouse_y)
+
+                    # Do the difference between before and after, and add it to the offset
+                    game_state.world_offset_x += mouseworld_x_before - mouseworld_x_after
+                    game_state.world_offset_y += mouseworld_y_before - mouseworld_y_after
+
+                elif event.button == 1:
+                    # PAN START
+                    game_state.panning = True
+                    game_state.pan_start_pos = mouse_x, mouse_y
+
+            elif event.type == pg.MOUSEBUTTONUP:
+                if event.button == 1 and game_state.panning:
+                    # PAN STOP
+                    game_state.panning = False
+
+        if game_state.panning:
+            # Pans the screen if the left mouse button is held
+            game_state.world_offset_x -= (mouse_x - game_state.pan_start_pos[0]) / game_state.zoom
+            game_state.world_offset_y -= (mouse_y - game_state.pan_start_pos[1]) / game_state.zoom
+            game_state.pan_start_pos = mouse_x, mouse_y
+
+        # Draw the screen
+        if game_state.tab == 1:
+            if game_state.update_screen:
+                # Updates the legacy screen if something has changed in the image data
+                game_state.legacy_screen.blit(sprite_sheet, (0, 0))
+                game_state.update_screen = False
+
+            # Sets variables for the section of the legacy screen to be zoomed
+            world_left, world_top = screen_2_world(0, 0)
+            world_right, world_bottom = SCREEN_WIDTH/game_state.zoom, SCREEN_HEIGHT/game_state.zoom
+
+            # Makes a temp surface with the dimensions of a smaller section of the legacy screen (for zooming).
+            new_screen = pg.Surface((world_right, world_bottom))
+            # Blits the smaller section of the legacy screen to the temp screen
+            new_screen.blit(game_state.legacy_screen, (0, 0), (world_left, world_top, world_right+1, world_bottom+1))
+            # Blits the final cut-out to the main screen, and scales the image to fit with the screen height and width
+            screen.fill((255, 255, 255))
+            screen.blit(pg.transform.scale(new_screen, (int(SCREEN_WIDTH+game_state.zoom), int(SCREEN_HEIGHT+game_state.zoom))), (-(world_left%1)*game_state.zoom, -(world_top%1)*game_state.zoom))
+            try:
+                mouse_x,mouse_y = pg.mouse.get_pos()
+                pg.display.set_caption(f'RGBA: {screen.get_at((mouse_x,mouse_y))}')
+                
+            except:
+                pass
+
+        # looping
+        pg.display.update()
+        clock.tick(30)
+except pg.error:
+    pass
+except Exception as e:
+    print(e)
